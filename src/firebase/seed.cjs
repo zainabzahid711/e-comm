e0211c0343e129ae63e70c78c6a40e4afbda9faf
Zainab Zahid <108534854+zainabzahid711@users.noreply.github.com>
2025-01-18 09:44:44 +0500

    Moved sensitive Firebase credentials

--- src/firebase/seed.cjs
+++ src/firebase/seed.cjs
@@ -5,13 +5,13 @@ require("dotenv").config();
 
 console.log("API Key:", process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
 const firebaseConfig = {
-  apiKey: "AIzaSyCm-0jhXMhZFYa6DVEFtZ28rPGlYgtCuCE",
-  authDomain: "farochron-b8de9.firebaseapp.com",
-  projectId: "farochron-b8de9",
-  storageBucket: "farochron-b8de9.firebasestorage.app",
-  messagingSenderId: "378284856303",
-  appId: "1:378284856303:web:86991ce3edf0e8c0b69c1a",
-  measurementId: "G-F91KWZSNSL",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
