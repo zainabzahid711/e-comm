c40fdc14a408129548746f50e7f7f5826563032c
Zainab Zahid <108534854+zainabzahid711@users.noreply.github.com>
2025-02-24 16:17:25 +0500

    color pallete changed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,8 +16,15 @@ export default {
         stylish: ['"Playfair Display"', "serif"],
       },
       colors: {
-        background: "#2C3E50",
-        foreground: "#333333",
+        background: "#FFFFFF", // Main background & Navbar
+        navbar: "#FFFFFF", // Explicitly setting navbar color
+        dropdownLeft: "#F7F7F7", // Left dropdown menu
+        dropdownRight: "#FFFFFF", // Right dropdown menu
+        text: "#000000", // Black text
+        cream: "#ECEBDE", // Lightest shade
+        beige: "#D7D3BF", // Warm neutral
+        sand: "#C1BAA1", // Medium earthy tone
+        olive: "#A59D84",
       },
       animation: {
         blink: "blink 1s step-end infinite",
